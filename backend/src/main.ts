d98a8d2802e6303f131908c1be189faaebc270f2
Abhijith H R <abhijith.hr@ib-systems.org>
2024-02-26 11:59:52 +0100

    removed seperate modules

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,10 +9,10 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Using NestJS built-in CORS support with corrected origin format
-  app.enableCors({
+  app.use(cors({
     origin: [process.env.CORS_ORIGIN],
     credentials: true,
-  });
+  }));
   app.use(cookieParser());
   await app.listen(4001);
 }
