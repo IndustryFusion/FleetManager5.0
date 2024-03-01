e12acd5a7726d043cc5e13813d4ead1206fc653a
bhavanas-ib <bhavana.s@ib-systems.org>
2024-03-01 23:30:16 +0530

    feat: search on templates

--- frontend/src/pages/templates.tsx
+++ frontend/src/pages/templates.tsx
@@ -18,12 +18,14 @@ type Template = {
   title: string;
   description: string;
 };
+const initialTemplates: Template[] = [];
 
 const cardTitleStyle = { fontSize: "17px", color: "rgb(0, 51, 0)", padding: "1rem, 2rem, 1rem, 2rem" };
 const BACKEND_API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 
 const GetListTemplate: React.FC = () => {
-  const [templates, setTemplates] = useState<Template[]>([]);
+  const [templates, setTemplates] = useState<Template[]>(initialTemplates);
+  const [filteredTemplates, setFilteredTemplates] = useState<Template[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [searchTemplate, setSearchTemplate] = useState("");
   const router = useRouter();
@@ -75,7 +77,7 @@ const GetListTemplate: React.FC = () => {
       return <div>Loading...</div>; // or any other loading/error state
     }
 
-    const filteredTemplates = templates.filter(
+    const filteredTemplate = filteredTemplates || templates.filter(
       (template) =>
         filter === "All" ||
         template.title.toLowerCase().includes(filter.toLowerCase())
@@ -86,7 +88,7 @@ const GetListTemplate: React.FC = () => {
 
     return (
       <div className="grid">
-        {filteredTemplates.map((template) => (
+        {filteredTemplate.map((template) => (
           <div key={template.id} className={`${columnClass}`}>
             <Card
               title={<span style={cardTitleStyle}>{template.title}</span>}
@@ -120,55 +122,48 @@ const GetListTemplate: React.FC = () => {
   );
 
   //search functionality
-const onFilter =(e: React.ChangeEvent<HTMLInputElement>)=>{
-  const value = e.target.value;
-  setSearchTemplate(value);
+  const onFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchTemplate(value);
 
-  if(value.length === 0){
-    console.log("is coming inside");
-    console.log(templates);
-    
-    setTemplates(templates);
-  }else{
-    const filtered = value.length > 0 ? 
-    templates?.filter((template) =>{   
-      return(
-        template?.title.toLowerCase().includes(value.toLowerCase())
-      )
-    }): templates;
-  setTemplates(filtered);
+    if (value.length === 0) {
+      setFilteredTemplates(null)
+    } else {
+      const filtered = value.length > 0 ?
+        templates?.filter((template) => {
+          return (
+            template?.title.toLowerCase().includes(value.toLowerCase())
+          )
+        }) : templates;
+      setFilteredTemplates(filtered)
+    }
+
   }
- 
-}
-
-
 
   return (
     <BlockUI blocked={loading}>
       <HorizontalNavbar />
       <Toast ref={toast} />
       <div style={{ padding: "1rem 1rem 2rem 3rem", zoom: "85%" }}>
-        <div className="flex gap-2">
+        <div className="flex gap-5">
           <p className="hover" style={{ fontWeight: "bold", fontSize: "1.5rem", marginTop: "80px" }}>
             Asset Templates
           </p>
-          <div style={{marginTop: "5rem"}}>
-            
-        <span className="p-input-icon-left">
-          <i className="pi pi-search" />
-          <InputText
-            style={{ width: "30rem" }}
-            placeholder="Search by templates"
-            type="search"
-            value={searchTemplate}
-            onChange={onFilter}
-          />
-        </span>
-      </div>
+          <div style={{ marginTop: "5rem" }}>
+            <span className="p-input-icon-left">
+              <i className="pi pi-search" />
+              <InputText
+                style={{ width: "30rem" }}
+                placeholder="Search by templates"
+                type="search"
+                value={searchTemplate}
+                onChange={onFilter}
+              />
+            </span>
+          </div>
         </div>
         <div className="mt-4" style={{ width: "100%" }}>
           <div className="flex flex-column align-items-left">
-
             <Card className="border-gray-800 border-1 border-round-lg">
               <TabView className="-ml-3 -mt-5 ">
                 <TabPanel header={allTabHeader}>{renderGrid("All")}</TabPanel>
