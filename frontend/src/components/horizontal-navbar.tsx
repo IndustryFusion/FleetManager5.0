b3ac9f22aad05b9852d86e981655620875d0daa5
bhavanas-ib <bhavana.s@ib-systems.org>
2024-05-14 15:16:27 +0530

    added license to frontend files

--- frontend/src/components/horizontal-navbar.tsx
+++ frontend/src/components/horizontal-navbar.tsx
@@ -1,17 +1,27 @@
+// 
+// Copyright (c) 2024 IB Systems GmbH 
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License. 
+// 
+
 import React from "react";
 import "primeflex/primeflex.css";
 import { Button } from "primereact/button";
-import { Badge } from "primereact/badge";
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
-import { ColorPicker } from "primereact/colorpicker";
-// import AlertDetails from "@/alert/alertDetails";
 import { useState } from "react";
 import { CSSProperties } from "react";
-import { Image } from "primereact/image";
 import { useRouter } from "next/router";
-// import NavLink from "../components/nav-links";
-import { Tooltip } from 'primereact/tooltip';
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
 import { resetTimer, logout } from "@/redux/auth/authSlice";
