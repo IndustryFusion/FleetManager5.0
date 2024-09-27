0f7f2a94910789d1e500bf6accbb0eeffeeef8ec
lahari.jain <lahari.jain@ib-systems.org>
2024-09-27 23:15:15 +0200

    Verfy and Get certs endpoint added

--- backend/src/endpoints/certificate/certificate.controller.ts
+++ backend/src/endpoints/certificate/certificate.controller.ts
@@ -20,6 +20,35 @@ export class CertificateController {
     }
   }
 
+  @Get('get-company-certificates/:company_ifric_id')
+  async getCompanyCertificates(@Param('company_ifric_id') company_ifric_id: string, @Req() req: Request) {
+    try {
+      return await this.certificateService.getCompanyCertificates(company_ifric_id, req);
+    } catch(err) {
+      return { 
+        success: false, 
+        status: err?.response?.status,
+        message: 'Failed to fetch certificate',
+        error: err.message
+      };
+    }
+  }
+
+  @Post('verify-company-certificate')
+  async verifyCompanyCertificate(@Body() data: {company_ifric_id: string; certificate_data: string;}) {
+    try {
+      const response = await this.certificateService.verifyCompanyCertificate(data.company_ifric_id, data.certificate_data);
+      return response.data;  
+    } catch(err) {
+      return { 
+        success: false, 
+        status: err?.response?.status,
+        message: 'Failed to fetch certificate',
+        error: err.message
+      };
+    }
+  }
+
   @Post('create-asset-certificate')
   async generateAssetCertificate(@Body() data: CreateAssetCertificateDto, @Req() req: Request) {
     try {
@@ -31,6 +60,30 @@ export class CertificateController {
         message: 'Failed to generate certificate',
         error: err.message
       };
+    }
+  }
+
+  @Get('get-asset-certificates') 
+  async getAssetCertificates(@Query('asset_ifric_id') asset_ifric_id: string, @Query('company_ifric_id') company_ifric_id: string, @Req() req: Request) {
+    try {
+      return await this.certificateService.getAssetCertificates(asset_ifric_id, company_ifric_id, req);
+    } catch(err) {
+      return { 
+        success: false, 
+        status: err?.response?.status,
+        message: 'Failed to fetch certificate',
+        error: err.message
+      };
+    }
+  }
+
+  @Post('verify-asset-certificate')
+  async verifyAssetCertificate(@Body() data: {asset_ifric_id: string; certificate_data: string;}) {
+    try {
+      const response = await this.certificateService.verifyAssetCertificate(data.asset_ifric_id, data.certificate_data);
+      return response.data;  
+    } catch(err) {
+      throw err;
     }
   }
 }
--- backend/src/endpoints/certificate/certificate.service.ts
+++ backend/src/endpoints/certificate/certificate.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import axios from 'axios';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import axios, { Axios } from 'axios';
 
 @Injectable()
 export class CertificateService {
-  
+
   private readonly ifricRegistryUrl = process.env.IFRIC_REGISTRY_BACKEND_URL;
   private readonly ifxPlatformUrl = process.env.IFX_PLATFORM_BACKEND_URL;
   private readonly icidServiceUrl = process.env.ICID_SERVICE_BACKEND_URL;
@@ -11,25 +11,34 @@ export class CertificateService {
   async generateCompanyCertificate(company_ifric_id: string, expiry: Date, user_email: string, req: Request) {
     try {
 
+      const companyVerified = true;
+
+      if (!companyVerified) {
+        return {
+          "success": false,
+          "status": 422,
+          "message": "Company Verified Fail, update company with valid registration number"
+        }
+      }
+
       const registryHeaders = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'Authorization': req.headers['authorization']
       };
+
       // check whether the last created certificate is valid or expired or not
-      const checkLastCertificate = await axios.get(`${this.ifricRegistryUrl}/certificate/get-company-certificate/${company_ifric_id}`, {headers: registryHeaders});
-      
-      if(checkLastCertificate.data.length > 0) {
-        const verifyLastCertificate = await axios.post(`${this.ifricRegistryUrl}/certificate/verify-company-certificate`,{
+      const checkLastCertificate = await axios.get(`${this.ifricRegistryUrl}/certificate/get-company-certificate/${company_ifric_id}`, { headers: registryHeaders });
+
+      if (checkLastCertificate.data.length > 0) {
+        const verifyLastCertificate = await axios.post(`${this.ifricRegistryUrl}/certificate/verify-company-certificate`, {
           certificate_data: checkLastCertificate.data[0].certificate_data,
           company_ifric_id,
         }, {
-          headers: {
-            'Content-Type': 'application/json'
-          }
+          headers: registryHeaders
         });
-        
-        if(verifyLastCertificate.data.valid) {
+
+        if (verifyLastCertificate.data.valid) {
           return {
             success: false,
             status: 400,
@@ -38,28 +47,65 @@ export class CertificateService {
         }
       }
 
-      const response = await axios.post(`${this.ifricRegistryUrl}/certificate/create-company-certificate`,{
+      const response = await axios.post(`${this.ifricRegistryUrl}/certificate/create-company-certificate`, {
         company_ifric_id,
         expiry,
         user_email
+      }, {
+        headers: registryHeaders
+      });
+
+      return response.data;
+    } catch (err) {
+      if (err.response.status == 401) {
+        throw new UnauthorizedException();
+      }
+      throw new InternalServerErrorException(`Failed to create company certificate: ${err.message}`);
+    }
+  }
+
+
+  async getCompanyCertificates(company_ifric_id: string, req: Request) {
+    try {
+      const registryHeaders = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': req.headers['authorization']
+      };
+      const companyCertificates = await axios.get(`${this.ifricRegistryUrl}/certificate/get-company-certificate/${company_ifric_id}`, { headers: registryHeaders });
+      return companyCertificates;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+
+  async verifyCompanyCertificate(company_ifric_id: string, certificate_data: string) {
+    try {
+      const verifiedResponse = await axios.post(`${this.icidServiceUrl}/certificate/verify-company-certificate`, {
+        company_ifric_id,
+        certificate_data
       }, {
         headers: {
           'Content-Type': 'application/json'
         }
       });
-
-      return response.data;
-    } catch(err) {
+      return verifiedResponse;
+    } catch (err) {
       throw err;
     }
   }
 
   async generateAssetCertificate(company_ifric_id: string, asset_ifric_id: string, user_email: string, expiry: Date, req: Request) {
     try {
-      console.log("start")
+      const registryHeaders = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': req.headers['authorization']
+      };
       // check whether the last created certificate is valid or expired or not
       //in get asset certificate we are verifiying the company cert
-      const checkLastCertificate = await axios.get(`${this.ifxPlatformUrl}/certificate/get-asset-certificate?asset_ifric_id=${asset_ifric_id}&company_ifric_id=${company_ifric_id}`);
+      const checkLastCertificate = await axios.get(`${this.ifxPlatformUrl}/certificate/get-asset-certificate?asset_ifric_id=${asset_ifric_id}&company_ifric_id=${company_ifric_id}`,{headers: registryHeaders});
       console.log("checkLastCertificate", checkLastCertificate.data)
       if(checkLastCertificate.data.length > 0) {
         const verifyLastCertificate = await axios.post(`${this.icidServiceUrl}/certificate/verify-asset-certificate`,{
@@ -79,13 +125,13 @@ export class CertificateService {
           };
         }
       }
-      
       // check whether the user is admin for DPP
       const getUserDetails = await axios.get(`${this.ifricRegistryUrl}/auth/get-user-details`,{
         params: {
           user_email,
           company_ifric_id
-        }
+        }, 
+        headers: registryHeaders
       })
   
       if(getUserDetails.data.length > 0) {
@@ -93,11 +139,12 @@ export class CertificateService {
           params: {
             product_name: 'DPP Creator',
             user_id: getUserDetails.data[0]._id
-          }
+          },
+          headers: registryHeaders
         });
   
         if(getProductDetails.data.length) {
-          const userProductAccessGroup = await axios.get(`${this.ifricRegistryUrl}/auth/get-access-group/${getProductDetails.data[0].access_group_id}`);
+          const userProductAccessGroup = await axios.get(`${this.ifricRegistryUrl}/auth/get-access-group/${getProductDetails.data[0].access_group_id}`, {headers: registryHeaders});
           if(userProductAccessGroup.data && userProductAccessGroup.data.group_name !== 'admin') {
             return {
               "success": false,
@@ -120,13 +167,63 @@ export class CertificateService {
         asset_ifric_id,
         expiry,
       }, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
+        headers: registryHeaders
       });
 
       return response.data;
     } catch(err) {
+      if(err.response.status == 401) {
+        throw new UnauthorizedException();
+      }
+      throw new InternalServerErrorException(`Failed to create asset certificate: ${err.message}`);
+    }
+  }
+
+  async getAssetCertificates(asset_ifric_id: string, company_ifric_id: string, req: Request) {
+    try {
+      const registryHeaders = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': req.headers['authorization']
+      };
+      const response = await axios.get(`${this.ifxPlatformUrl}/certificate/get-asset-certificate`,
+        {
+          params: {
+            asset_ifric_id: asset_ifric_id,
+            company_ifric_id: company_ifric_id
+          },  
+          headers: registryHeaders
+        }
+      );
+      return response.data;
+    } catch (error: any) {
+      if (error?.response && error?.response?.status === 401) {
+        return {
+          "success": false,
+          "status": 401,
+          "message": "Login Expired"
+        }
+      } else {
+        return {
+          "success": false,
+          "status": 401,
+          "message": error.response?.data?.message || "Error fetching certificates"
+        }
+      }
+    }
+  }
+
+  async verifyAssetCertificate(asset_ifric_id: string, certificate_data: string) {
+    try {
+      return await axios.post(`${this.icidServiceUrl}/certificate/verify-company-certificate`, {
+        asset_ifric_id,
+        certificate_data
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    } catch (err) {
       throw err;
     }
   }
