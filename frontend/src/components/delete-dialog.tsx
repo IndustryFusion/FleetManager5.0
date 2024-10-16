41af3b51376b08945150741de34d1b5e41defb31
bhavanas-ib <bhavana.s@ib-systems.org>
2024-10-16 15:37:33 +0530

    added edit ,reset and delete dialog

--- frontend/src/components/delete-dialog.tsx
+++ frontend/src/components/delete-dialog.tsx
@@ -2,52 +2,54 @@ import { useTranslation } from "next-i18next";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import { Dispatch, SetStateAction } from "react";
-
+import "../../public/styles/add-contract.css"
 
 interface DeleteDialogProps {
-    deleteDialog: boolean;
-    setDeleteDialog: Dispatch<SetStateAction<boolean>>;
-    handleDelete: () => void;
-    deleteItemName?: string;
+  deleteDialog: boolean;
+  setDeleteDialog: Dispatch<SetStateAction<boolean>>;
+  handleDelete: () => void;
+  deleteItemName?: string;
 }
 
-const DeleteDialog: React.FC<DeleteDialogProps> = ({ deleteDialog, setDeleteDialog,  handleDelete, deleteItemName}) => {
-    const { t } = useTranslation('overview'); 
-    const header = () => <h4 className="m-0">{t('overview:confirmation')}</h4>
-    return (
-        <>
-            <Dialog
-            style={{width:"600px"}}
-                visible={deleteDialog}
-                onHide={() => setDeleteDialog(false)}
-                header={header}
-            >
-                <div className="mb-6">
-                    <span>{t('overview:deleteWarning')}</span>
-                    <span style={{ textTransform: "capitalize", fontWeight: "bold" }}> {deleteItemName}</span>
-                </div>
+const DeleteDialog: React.FC<DeleteDialogProps> = ({
+  deleteDialog,
+  setDeleteDialog,
+  handleDelete,
+  deleteItemName,
+}) => {
+  const { t } = useTranslation("overview");
+  const header = () => <h4 className="m-0">Confirm Delete</h4>;
+  return (
+    <>
+      <Dialog
+        style={{ width: "600px" }}
+        visible={deleteDialog}
+        onHide={() => setDeleteDialog(false)}
+        header={header}
+      >
+        <div className="mb-6 mt-5">
+          <h3 className="delete-text"> {deleteItemName}</h3>
+        </div>
 
-
-                <div className="flex justify-content-end">
-                    <Button
-                        label={t('yes')}
-                        icon="pi pi-check"
-                        className="mr-2"
-                        onClick={handleDelete}
-                    >
-                    </Button>
-                    <Button
-                        label={t('no')}
-                        icon="pi pi-times"
-                        severity="danger" outlined
-                        className="ml-3"
-                        onClick={() => setDeleteDialog(false)}
-                    ></Button>
-                </div>
-
-            </Dialog>
-        </>
-    )
-}
+        <div className="flex justify-content-end">
+          <Button
+            label={t("yes")}
+            icon="pi pi-check"
+            className="mr-2 delete-action-buttons"
+            onClick={handleDelete}
+          ></Button>
+          <Button
+            label={t("no")}
+            icon="pi pi-times"
+            severity="danger"
+            outlined
+            className="ml-3 delete-action-buttons"
+            onClick={() => setDeleteDialog(false)}
+          ></Button>
+        </div>
+      </Dialog>
+    </>
+  );
+};
 
 export default DeleteDialog;
