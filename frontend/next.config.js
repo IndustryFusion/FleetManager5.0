c291085c2e3cbca40d3174ad34b15148e49be96b
jragunanthan <ragunanthan.j@ib-systems.org>
2024-11-08 14:37:01 +0530

    disable eslint and typescript for build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -19,6 +19,17 @@
 const { i18n } = require('./next-i18next.config');
 const nextConfig = {
     i18n,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+    basePath: ''	
 }
 
 module.exports = nextConfig
