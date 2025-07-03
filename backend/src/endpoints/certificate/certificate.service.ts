2e46c5c42d1ebcf965e46f331c3c827179b67f09
Abhijith H R <abhijith.hr@ib-systems.org>
2025-07-03 16:23:36 +0200

    Fixes cert issue

--- backend/src/endpoints/certificate/certificate.service.ts
+++ backend/src/endpoints/certificate/certificate.service.ts
@@ -62,7 +62,7 @@ export class CertificateService {
           headers: registryHeaders
         });
 
-        if (verifyLastCertificate.data.valid) {
+        if (verifyLastCertificate.data) {
           return {
             success: false,
             status: 400,
@@ -115,16 +115,14 @@ export class CertificateService {
             const checkLastCertificate = await axios.get(`${this.ifricRegistryUrl}/certificate/get-company-certificate/${company_ifric_id}`, { headers: registryHeaders });
             
             if(checkLastCertificate.data.length > 0) {
-              const verifyLastCertificate = await axios.post(`${this.icidServiceUrl}/certificate/verify-company-certificate`,{
+              const verifyLastCertificate = await axios.post(`${this.ifricRegistryUrl}/certificate/verify-company-certificate`,{
                 certificate_data: checkLastCertificate.data[0].certificate_data,
                 company_ifric_id,
-              }, {
-                headers: {
-                  'Content-Type': 'application/json'
-                }
-              });
+              }, 
+                { headers: registryHeaders }
+              );
               
-              if(verifyLastCertificate.data.valid) {
+              if(verifyLastCertificate.data) {
                 return {
                   success: true,
                   status: 201,
@@ -164,16 +162,13 @@ export class CertificateService {
       const checkLastCertificate = await axios.get(`${this.ifxPlatformUrl}/certificate/get-asset-certificate?asset_ifric_id=${asset_ifric_id}&company_ifric_id=${company_ifric_id}`,{headers: ifxHeaders});
       console.log("checkLastCertificate", checkLastCertificate.data)
       if(checkLastCertificate.data.length > 0) {
-        const verifyLastCertificate = await axios.post(`${this.icidServiceUrl}/certificate/verify-asset-certificate`,{
+        const verifyLastCertificate = await axios.post(`${this.ifxPlatformUrl}/certificate/verify-asset-certificate`,{
           certificate_data: checkLastCertificate.data[0].certificate_data,
           asset_ifric_id,
-        }, {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
+        }, {headers: ifxHeaders}
+      );
         console.log("verifyLastCertificate", verifyLastCertificate.data)
-        if(verifyLastCertificate.data.valid) {
+        if(verifyLastCertificate.data) {
           return {
             success: false,
             status: 400,
@@ -309,7 +304,7 @@ export class CertificateService {
           }
         });
         console.log("verifyLastCertificate", verifyLastCertificate.data)
-        if(verifyLastCertificate.data.valid) {
+        if(verifyLastCertificate.data) {
           return verifyLastCertificate.data;
         }
       } else {
